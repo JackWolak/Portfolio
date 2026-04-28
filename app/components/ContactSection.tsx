b85a537a1ea0eea455d0f1f2f65c31cf4ec83628
Jack Wolak <112406420+JackWolak@users.noreply.github.com>
2026-04-28 11:14:46 -0400

    Update ContactSection.tsx
    Update location

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -21,7 +21,7 @@ export default function ContactSection() {
 							/>
 							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 						</svg>
-						<span>Jenison, MI</span>
+						<span>Wendell, NC</span>
 					</div>
 
 					{/* Contact Buttons */}
